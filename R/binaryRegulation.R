f1ac43cda3ddad71893b2dc33b3908212047b28f
yuxiansen <rmyhandsome@163.com>
2022-05-21 16:46:46 +0800

    Update binaryRegulation.R

--- R/binaryRegulation.R
+++ R/binaryRegulation.R
@@ -1,16 +1,16 @@
 #' @title Binary Regulatory in Local Heterogeneous network
 #' @description Locate the experimental binary regulatory data among biological elements in regional Heterogeneous network which is integrated from a wide range of bioinformatics databases.
 #'
-#' @param transcriptomeList a vector consist of transcriptome including circRNA, lncRNA, miRNA ,mRNA or KEGG/Reactome/Wikipathway pathway IDs.
+#' @param elementList a vector consist of elements including circRNA, lncRNA, miRNA ,mRNA or KEGG/Reactome/Wikipathway pathway IDs.
 #' @param regulationType character string naming the type of regulation which can be located including "circRNA-miRNA", "lncRNA-miRNA", "lncRNA-mRNA", "miRNA-mRNA", "miRNA-pathway", "mRNA-pathway".
 #' @param allRegulation logical value indicates locating all related regulations from the storage or internal regulations between input elements.
 #'
 #' @examples
-#' binaryRegulation(transcriptomeList = c("2309", "3838", "MIMAT0000255"), regulationType = "miRNA-mRNA",  allRegulation = FALSE)
+#' binaryRegulation(elementList = c("2309", "3838", "MIMAT0000255"), regulationType = "miRNA-mRNA",  allRegulation = FALSE)
 #' @export
-binaryRegulation = function(transcriptomeList = NULL, regulationType = c("circRNA-miRNA", "lncRNA-miRNA", "lncRNA-mRNA", "miRNA-mRNA", "miRNA-pathway", "mRNA-pathway"), allRegulation = FALSE) {
+binaryRegulation = function(elementList = NULL, regulationType = "miRNA-mRNA", allRegulation = FALSE) {
 
-  if(is.null(transcriptomeList)){
+  if(is.null(elementList)){
     return("Please enter at least one element......")
   }
 
@@ -18,11 +18,11 @@ binaryRegulation = function(transcriptomeList = NULL, regulationType = c("circRN
     return("Please choose a regulation type from circRNA-miRNA, lncRNA-miRNA, lncRNA-mRNA, miRNA-mRNA, miRNA-pathway, mRNA-pathway")
   }
   load(system.file("extdata", "netelements.rda", package = "NetLCP"))
-  if(!all(transcriptomeList %in% NETELEMENTS)){
-    print(paste0("Filtering the missing input transcriptome in input network......"))
-    print(paste0(paste0(transcriptomeList[!(transcriptomeList %in% NETELEMENTS)], collapse = "/"), " have been filtered....."))
-    transcriptomeList = transcriptomeList[transcriptomeList %in% NETELEMENTS]
-    print(paste0("Now remain ", length(transcriptomeList)))
+  if(!all(elementList %in% NETELEMENTS)){
+    print(paste0("Filtering the missing input elements in input network......"))
+    print(paste0(paste0(elementList[!(elementList %in% NETELEMENTS)], collapse = "/"), " have been filtered....."))
+    elementList = elementList[elementList %in% NETELEMENTS]
+    print(paste0("Now remain ", length(elementList)))
   }
 
   print("Binary Regulation extraction begins, please wait while we do something......")
@@ -31,16 +31,16 @@ binaryRegulation = function(transcriptomeList = NULL, regulationType = c("circRN
   REG =  REG %>% dplyr::filter(regType == regulationType)
 
   if(allRegulation == TRUE){
-    ExtractedData = REG[REG$node1 %in% transcriptomeList | REG$node2 %in% transcriptomeList,]
+    ExtractedData = REG[REG$node1 %in% elementList | REG$node2 %in% elementList,]
     if(nrow(ExtractedData) == 0){
-      return("Sorry, search result is empty, please try to check the ID types of input transcriptome or the regulationtype......")
+      return("Sorry, search result is empty, please try to check the ID types of input elements or the regulationtype......")
     }else{
       return(ExtractedData)
     }
   }else{
-    ExtractedData = REG[REG$node1 %in% transcriptomeList & REG$node2 %in% transcriptomeList,]
+    ExtractedData = REG[REG$node1 %in% elementList & REG$node2 %in% elementList,]
     if(nrow(ExtractedData) == 0){
-      return("Sorry, search result is empty, please try to check the ID types of input transcriptome or the regulationtype......")
+      return("Sorry, search result is empty, please try to check the ID types of input elements or the regulationtype......")
     }else{
       return(ExtractedData)
     }
